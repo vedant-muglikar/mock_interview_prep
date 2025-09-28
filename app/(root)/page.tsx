9596d32946bc01720b8575d7c59d0015b6df91ef
vedant-muglikar <vedantmuglikar1@gmail.com>
2025-09-28 15:57:59 +0530

    home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,56 @@
+import InterviewCard from "@/components/InterviewCard";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="min-h-screen p-8">
-      <h1 className="text-4xl font-bold text-white mb-4">
-        Welcome to PrepWise
-      </h1>
-      <p className="text-lg text-light-100">
-        AI-powered mock interview preparation platform
-      </p>
-    </div>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+
+          <p className="text-lg">
+            Practice on real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href={"/interview"}>Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"></Image>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+
+          {/* {<p>You haven&apos;t taken any interviews yet</p>} */}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
