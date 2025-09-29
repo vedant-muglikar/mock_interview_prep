4fa711d623f2b5cf0c9978058d36c589b09a8f03
vedant-muglikar <vedantmuglikar1@gmail.com>
2025-09-29 11:02:21 +0530

    sign in and up

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 
 const initFirebaseAdmin = () => {
   const apps = getApps();
